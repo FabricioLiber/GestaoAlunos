fbea0fb5143839fe765ba48fb4f8a27f10baacce
Fabrício Liberato <fabricio.liber@gmail.com>
2018-09-16 00:06:54 -0300

    Atualizando url

--- projeto/settings.py
+++ projeto/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    'statics',
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'gestaoalunos.herokuapp.com/media/'
+MEDIA_ROOT = 'media'
